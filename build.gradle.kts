d738f1f86a6e20e36250ac93e4d6839b0d1ad52e
2018007956 <bbmt1906@hanyang.ac.kr>
2025-04-22 15:03:37 +0900

    feat: set dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,19 @@
+buildscript {
+    dependencies {
+        classpath("io.spring.gradle:dependency-management-plugin:1.0.15.RELEASE")
+    }
+}
+
 plugins {
-    id("java")
+    java
+    `java-library`
+    id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 group = "org.example"
@@ -10,8 +24,18 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("javax.persistence:javax.persistence-api:2.2")
+
+    testImplementation("com.tngtech.archunit:archunit:1.0.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
