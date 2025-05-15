8b04534f1b714b35921f2db26d7ee017e275a9c5
2018007956 <bbmt1906@hanyang.ac.kr>
2025-05-15 09:35:41 +0900

    feat: membership service

---
+++ Membership/src/main/java/com/payserver/membership/adapter/in/web/RegisterMembershipController.java
@@ -0,0 +1,33 @@
+package com.payserver.membership.adapter.in.web;
+
+import com.payserver.membership.application.port.in.RegisterMembershipCommand;
+import com.payserver.membership.application.port.in.RegisterMembershipUseCase;
+import com.payserver.membership.domain.Membership;
+import common.WebAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+public class RegisterMembershipController {
+
+    private final RegisterMembershipUseCase registerMembershipUseCase;
+    @PostMapping(path = "/membership/register")
+    Membership registerMembership(@RequestBody RegisterMembershipRequest request) {
+
+        // request -> Command (추상화 계층)
+        RegisterMembershipCommand command = RegisterMembershipCommand.builder()
+                .name(request.getName())
+                .address(request.getAddress())
+                .email(request.getEmail())
+                .isValid(true)
+                .isCorp(request.isCorp())
+                .build();
+
+        // Usecase
+        return registerMembershipUseCase.registerMembership(command);
+    }
+}
---
+++ Membership/src/main/java/com/payserver/membership/adapter/out/persistence/MembershipPersistenceAdapter.java
@@ -0,0 +1,32 @@
+package com.payserver.membership.adapter.out.persistence;
+
+import com.payserver.membership.application.port.out.RegisterMembershipPort;
+import com.payserver.membership.domain.Membership;
+import common.PersistenceAdapter;
+import lombok.RequiredArgsConstructor;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+class MembershipPersistenceAdapter implements RegisterMembershipPort {
+
+    private final SpringDataMembershipRepository membershipRepository;
+    @Override
+    // DB 연동
+    public MembershipJpaEntity createMembership(Membership.MembershipName membershipName
+            , Membership.MembershipEmail membershipEmail
+            , Membership.MembershipAddress membershipAddress
+            , Membership.MembershipIsValid membershipIsValid
+            , Membership.MembershipIsCorp membershipIsCorp
+    ){
+        membershipRepository.save(
+                new MembershipJpaEntity(
+                        membershipName.getNameValue(),
+                        membershipEmail.getEmailValue(),
+                        membershipAddress.getAddressValue(),
+                        membershipIsValid.isValidValue(),
+                        membershipIsCorp.isCorpValue()
+                )
+        );
+        return null;
+    }
+}
